6630d2a30f4fbe42213d4d5adfab028a5dee57c6
MalakHenendeveloper <MalakHenendeveloper@gmail.com>
2026-06-21 18:17:07 +0300

    Fix Vercel MongoDB connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,30 @@
-// const mongoose = require('mongoose');
-// const config = require('./env');
+// // const mongoose = require('mongoose');
+// // const config = require('./env');
+
+// // const connectDB = async () => {
+// //   try {
+// //     const conn = await mongoose.connect(config.mongoose.url, config.mongoose.options);
+// //     console.log(`MongoDB Connected: ${conn.connection.host}`);
+// //   } catch (error) {
+// //     console.error(`Database connection error: ${error.message}`);
+// //     process.exit(1);
+// //   }
+// // };
+
+// // module.exports = connectDB;
+// const mongoose = require("mongoose");
+// const config = require("./env");
 
 // const connectDB = async () => {
 //   try {
-//     const conn = await mongoose.connect(config.mongoose.url, config.mongoose.options);
+//     console.log("=== CONNECT DB CALLED ===");
+//     console.log("Mongo URI Exists:", !!config.mongoose.url);
+
+//     const conn = await mongoose.connect(
+//       config.mongoose.url,
+//       config.mongoose.options,
+//     );
+
 //     console.log(`MongoDB Connected: ${conn.connection.host}`);
 //   } catch (error) {
 //     console.error(`Database connection error: ${error.message}`);
@@ -17,18 +38,24 @@ const config = require("./env");
 
 const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState >= 1) {
+      return mongoose.connection;
+    }
+
     console.log("=== CONNECT DB CALLED ===");
     console.log("Mongo URI Exists:", !!config.mongoose.url);
 
-    const conn = await mongoose.connect(
-      config.mongoose.url,
-      config.mongoose.options,
-    );
+    const conn = await mongoose.connect(config.mongoose.url, {
+      serverSelectionTimeoutMS: 5000,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    return conn;
   } catch (error) {
-    console.error(`Database connection error: ${error.message}`);
-    process.exit(1);
+    console.error("DATABASE CONNECTION FAILED");
+    console.error(error);
+    throw error;
   }
 };
 
